588f5c564a71c43b69d90a029408eb2f22543ec2
AntoineFEREY <kwodhan@protonmail.com>
2026-04-05 18:26:44 +0200

    Seriously Dude ?

--- front/src/app/components/document/document-validation-dialog/document-validation-dialog.component.ts
+++ front/src/app/components/document/document-validation-dialog/document-validation-dialog.component.ts
@@ -1,11 +1,16 @@
-import {Component, computed, inject, Signal, signal} from '@angular/core';
+import { Component, computed, inject, Signal, signal } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import {DocumentMigrationResult, ReportMigrationResult, ValidationIssue, ValidationResult} from '@shared/types';
-import {DocumentService} from '../../../services/document.service';
-import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {
+  DocumentMigrationResult,
+  ReportMigrationResult,
+  ValidationIssue,
+  ValidationResult,
+} from '@shared/types';
+import { DocumentService } from '../../../services/document.service';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 interface DialogData {
   validation: ValidationResult | null;
@@ -29,18 +34,17 @@ export class DocumentValidationDialogComponent {
   success = signal(false);
   error = signal<string | null>(null);
   idTarget = signal<string | null>(null);
-  blockingIssues:Signal<ValidationIssue[]> = computed(() => {
+  blockingIssues: Signal<ValidationIssue[]> = computed(() => {
     const v = this.validation();
     if (v) {
-      return v.issues.filter(i => i.severity === 'BLOCKING');
+      return v.issues.filter((i) => i.severity === 'BLOCKING');
     }
     return [];
-
   });
-  warningIssues:Signal<ValidationIssue[]> = computed(() => {
+  warningIssues: Signal<ValidationIssue[]> = computed(() => {
     const v = this.validation();
     if (v) {
-      return v.issues.filter(i => i.severity === 'WARNING');
+      return v.issues.filter((i) => i.severity === 'WARNING');
     }
     return [];
   });
@@ -55,35 +59,39 @@ export class DocumentValidationDialogComponent {
 
   onUpdateReport(): void {
     this.loading.set(true);
-    this.documentService.getReportMigrateDocument(this.data.repoNameSource,this.data.repoNameTarget,this.data.docId).subscribe({
-      next: (result: ReportMigrationResult) => {
-        this.validation.set(result.validation);
-        this.loading.set(false);
-      },
-      error: () => {
-        this.success.set(false);
-        this.error.set("Why are you gay ?");
-        this.loading.set(false);
-      },
-    });
+    this.documentService
+      .getReportMigrateDocument(this.data.repoNameSource, this.data.repoNameTarget, this.data.docId)
+      .subscribe({
+        next: (result: ReportMigrationResult) => {
+          this.validation.set(result.validation);
+          this.loading.set(false);
+        },
+        error: () => {
+          this.success.set(false);
+          this.error.set(`Document ${this.data.docId} Validating failed.`);
+          this.loading.set(false);
+        },
+      });
   }
 
   onMigrate(): void {
     this.loading.set(true);
-    this.documentService.migrateDocument(this.data.repoNameSource,this.data.repoNameTarget,this.data.docId).subscribe({
-      next: (result: DocumentMigrationResult) => {
-        this.validation.set(result.validation);
-        this.success.set(result.success);
-        this.error.set(result.error ?? null);
-        this.idTarget.set(result.id ?? null);
-        this.hasMigrated.set(result.success);
-        this.loading.set(false);
-      },
-      error: () => {
-        this.success.set(false);
-        this.error.set("Why are you gay ?");
-        this.loading.set(false);
-      },
-    });
+    this.documentService
+      .migrateDocument(this.data.repoNameSource, this.data.repoNameTarget, this.data.docId)
+      .subscribe({
+        next: (result: DocumentMigrationResult) => {
+          this.validation.set(result.validation);
+          this.success.set(result.success);
+          this.error.set(result.error ?? null);
+          this.idTarget.set(result.id ?? null);
+          this.hasMigrated.set(result.success);
+          this.loading.set(false);
+        },
+        error: () => {
+          this.success.set(false);
+          this.error.set(`Document ${this.data.docId} Migrating failed.`);
+          this.loading.set(false);
+        },
+      });
   }
 }
