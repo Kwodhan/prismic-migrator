5f973c22ea9a35261cdf8fcc49cc3daa17d04b25
AntoineFEREY <kwodhan@protonmail.com>
2026-05-04 14:49:50 +0200

    Help menu

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,6 +6,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
 import { AuthService } from './services/auth.service';
+import { EnvironmentService } from './services/environment.service';
+import { Environment } from '@shared/types';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,20 @@ import { AuthService } from './services/auth.service';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly auth = inject(AuthService);
+  private readonly environmentService = inject(EnvironmentService);
+  protected readonly environments = signal<Environment[]>([]);
+
+  ngOnInit(): void {
+    this.loadEnvironments();
+  }
+
+  private loadEnvironments(): void {
+    this.environmentService.getEnvironments().subscribe({
+      next: (envs) => {
+        this.environments.set(envs);
+      },
+    });
+  }
 }
